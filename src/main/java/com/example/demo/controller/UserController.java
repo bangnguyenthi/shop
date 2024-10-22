ded83d37f41501f61b6fa166ae1d5fc474016ed1
MAC <mac@MacBook-Air-cua-MAC.local>
2024-10-22 23:11:43 +0700

    shop

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class UserController {
+    private UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test=this.userService.handleHello();
+        return "eric"; 
+    }
+
+}
+
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello(); 
+//     }
+
+// }
